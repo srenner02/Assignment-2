2998718d6d246c26317e86657dbbd17d13e3d190
Samuel Renner <113732701+srenner02@users.noreply.github.com>
2022-10-14 19:46:08 +0000

    Added validation for username vs array

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function validateForm() {
-  //pulls status value from lab 5 form and assigns to x
+  //pulls username value from page3 form and assigns it to z
+  let z = document.forms["page3Form"]["username"].value;
+  //assigns required values in prompt to array usernames
+  const usernames = ["Harry", "Elizabeth", "Shana"]
+  //checks username vs each item of the array and returns false if a match is found
+  for(const a of usernames){
+    if (a == z) {
+    alert("Username already exists");
+    return false;
+    }
+  }
+  
+  //pulls password and password2 value from page3 form and assigns to x and y
   let x = document.forms["page3Form"]["password"].value;
   let y = document.forms["page3Form"]["password2"].value;
   //if x does not match y display alert
